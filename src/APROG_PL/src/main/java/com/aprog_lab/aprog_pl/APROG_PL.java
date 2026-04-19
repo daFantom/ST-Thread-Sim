6cdd66bacc8259245dff3679f1bfa5647ad1855d
Fantom <emanuel.baciu@edu.uah.es>
2026-04-19 16:52:13 +0200

    Added initial docs and created Netbeans project

---
+++ src/APROG_PL/src/main/java/com/aprog_lab/aprog_pl/APROG_PL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aprog_lab.aprog_pl;
+
+/**
+ *
+ * @author Emanuel Baciu
+ */
+public class APROG_PL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
