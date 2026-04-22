06618216cf4f7e3760bad857dea801396a6eb0e5
Fantom <emanuel.baciu@edu.uah.es>
2026-04-22 18:58:41 +0200

    Added Safe zones and Unsafe zones.

--- src/APROG_PL/src/main/java/com/aprog_lab/aprog_pl/Main.java
+++ src/APROG_PL/src/main/java/com/aprog_lab/aprog_pl/Main.java
@@ -1,5 +1,6 @@
 package com.aprog_lab.aprog_pl;
 
+import java.util.ArrayList;
 import java.util.concurrent.CyclicBarrier;
 /**
  *
@@ -8,8 +9,22 @@ import java.util.concurrent.CyclicBarrier;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Unsafe_Zone> uz = new ArrayList<>();                              // Children and Demogorgons share this ArrayList.
+        ArrayList<Safe_Zone> sz = new ArrayList<>();                                // ONLY children are allowed to use this ArrayList.
         
-        try
+        Unsafe_Zone Forest = new Unsafe_Zone("Forest");
+        Unsafe_Zone Lab = new Unsafe_Zone("Laboratory");
+        Unsafe_Zone Mall = new Unsafe_Zone("Shopping Mall");
+        Unsafe_Zone Sewer = new Unsafe_Zone("Sewer");
+        Unsafe_Zone Hive = new Unsafe_Zone("HIVE");
+        
+        Safe_Zone ms = new Safe_Zone("Hawkin's Main Street");
+        Safe_Zone bb = new Safe_Zone("Bayer's Basement");
+        Safe_Zone radio = new Safe_Zone("WSQK Radio");
+        
+        uz.add(Forest); uz.add(Lab); uz.add(Mall); uz.add(Sewer); uz.add(Hive);
+        sz.add(ms); sz.add(bb); sz.add(radio);
+        try                                                                            // Initial threads (Alpha Demog and Children) creation.
         {
             int idn = 0;
             new Demogorgon("D"+String.format("%04d",idn), 0).start();
